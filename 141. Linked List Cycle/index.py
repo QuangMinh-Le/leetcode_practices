4f465edc15a04ff7c4671ad5d5b80eacbc80f002
Minh <quangminh.ottawa@gmail.com>
2024-10-15 15:28:25 -0400

    solved new problem .141

---
+++ 141. Linked List Cycle/index.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+   def __init__(self, x):
+      self.val = x
+      self.next = None
+
+class Solution:
+   def hasCycle(self, head: Optional[ListNode]) -> bool:
+      # Check if there is no node or only 1 node in the list
+      if not head or not head.next:
+          return False
+      
+      slow = head
+      fast = head.next
+      while slow != fast:
+          if not fast or not fast.next:
+              return False
+          slow = slow.next
+          fast = fast.next.next
+      return True
