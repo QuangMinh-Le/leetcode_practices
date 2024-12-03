eae57906d5cac1e7220f66071f7bf9e230bbf982
Minh <quangminh.ottawa@gmail.com>
2024-12-03 11:49:50 -0500

    solved new problem

---
+++ 1251. Average Selling Price/index.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT  
+   Prices.product_id
+   ,coalesce( ROUND(SUM(Prices.price * UnitsSold.units) / Sum(UnitsSold.units), 2) , 0) As average_price
+FROM Prices
+Left JOIN UnitsSold
+ON (UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date)
+and Prices.product_id = UnitsSold.product_id
+Group by Prices.product_id
